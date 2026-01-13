1c73d4cd817ea1d6e1a411f5e2c95c71291b828a
Hernan Rotellini <hernanrotellini@hotmail.com>
2026-01-13 11:55:22 -0300

    update client

--- models/client.py
+++ models/client.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Boolean  # ✅ Importar Boolean
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel
@@ -12,7 +12,10 @@ class ClientModel(BaseModel):
     email = Column(String, unique=True, index=True)
     telephone = Column(String)
     password = Column(String)
+    
+   
+    is_admin = Column(Boolean, default=False)
 
     addresses = relationship("AddressModel", back_populates="client", cascade="all, delete-orphan", lazy="select")
     orders = relationship("OrderModel", back_populates="client", lazy="select")
-    bills = relationship("BillModel", back_populates="client", lazy="select")  # ✅ Added
+    bills = relationship("BillModel", back_populates="client", lazy="select")
